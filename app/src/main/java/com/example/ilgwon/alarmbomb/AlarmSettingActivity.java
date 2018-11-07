6e5e58ba8fadf68ac9ed17875677e8589ec6260a
hihyun <hyunhi1123@gmail.com>
2018-11-07 14:27:35 +0900

    유아이 하는중

---
+++ app/src/main/java/com/example/ilgwon/alarmbomb/AlarmSettingActivity.java
@@ -0,0 +1,23 @@
+package com.example.ilgwon.alarmbomb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class AlarmSettingActivity extends AppCompatActivity {
+    static final String[] LIST_MENU = {"TEST1", "TEST2", "TEST3"} ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_setting);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU) ;
+        ListView listview = (ListView) findViewById(R.id.alarm_list) ;
+        listview.setAdapter(adapter) ;
+
+
+
+
+    }
+}
