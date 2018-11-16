57e6d418ade9b186fd33e5303b4addb34f1d5087
ilgwonPark2 <ilgwon@interactor.com>
2018-11-16 18:30:19 +0900

    Alarm Setting2 Layout, Java files to work well.
    - Intent communication between AlarmAddActivity and AlarmSettingActivity.

---
+++ app/src/main/java/com/example/ilgwon/alarmbomb/AlarmAddActivity.java
@@ -0,0 +1,67 @@
+package com.example.ilgwon.alarmbomb;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+public class AlarmAddActivity extends Activity {
+    TimePicker timePickerAlarmTime;
+    Button btnAddAlarm;
+    GregorianCalendar currentCalendar = new GregorianCalendar(TimeZone.getTimeZone("GMT+09:00"));
+    public static final int DEFAULT_ALARM_REQUEST = 800;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_add);
+
+        btnAddAlarm = (Button) findViewById(R.id.btnAddAlarm);
+        timePickerAlarmTime = (TimePicker) findViewById(R.id.timePickerAlarmTime);
+        timePickerAlarmTime.setIs24HourView(false);
+
+
+        btnAddAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hh = timePickerAlarmTime.getHour();
+                int mm = timePickerAlarmTime.getMinute();
+                //                int reqCode = DEFAULT_ALARM_REQUEST + arrayListAlarmTimeItem.size();
+                int reqCode = DEFAULT_ALARM_REQUEST;
+                //                int i = arrayListAlarmTimeItem.size();
+                GregorianCalendar gregorianCalendar = new GregorianCalendar(TimeZone.getTimeZone("GMT+09:00"));
+
+                int currentYY = currentCalendar.get(Calendar.YEAR);
+                int currentMM = currentCalendar.get(Calendar.MONTH);
+                int currentDD = currentCalendar.get(Calendar.DAY_OF_MONTH);
+
+                gregorianCalendar.set(currentYY, currentMM, currentDD, hh, mm, 00);
+
+                if (gregorianCalendar.getTimeInMillis() < currentCalendar.getTimeInMillis()) {
+                    gregorianCalendar.set(currentYY, currentMM, currentDD + 1, hh, mm, 00);
+                    Log.i("TAG", gregorianCalendar.getTimeInMillis() + ":");
+                }
+//                String abc = ((String) hh);
+//                Toast.makeText(this,"result "+time, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(AlarmAddActivity.this, AlarmSettingActivity.class);
+                intent.putExtra("time", hh + ":" + mm);
+//                intent.putExtra("data", "dd: " + currentCalendar.getTime().toLocaleString());
+                intent.putExtra("reqCode", reqCode);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/ilgwon/alarmbomb/AlarmSettingActivity.java
+++ app/src/main/java/com/example/ilgwon/alarmbomb/AlarmSettingActivity.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,11 +28,11 @@ public class AlarmSettingActivity extends AppCompatActivity {
     public static final int DEFAULT_ALARM_REQUEST = 800;
 
 
-    TimePicker timePickerAlarmTime;
+//    TimePicker timePickerAlarmTime;
     Button btnAddAlarm;
     ListView listViewAlarm;
     ArrayList<AlarmData> arrayListAlarmTimeItem = new ArrayList<AlarmData>();
-    GregorianCalendar currentCalendar = new GregorianCalendar(TimeZone.getTimeZone("GMT+09:00"));
+//    GregorianCalendar currentCalendar = new GregorianCalendar(TimeZone.getTimeZone("GMT+09:00"));
     private SharedPreferences sharedPref;
     private SharedPreferences.Editor sharedEditor;
 
@@ -51,69 +52,99 @@ public class AlarmSettingActivity extends AppCompatActivity {
 
         mContext = getApplicationContext();
 
-        timePickerAlarmTime = (TimePicker)findViewById(R.id.timePickerAlarmTime);
-        btnAddAlarm = (Button)findViewById(R.id.btnAddAlarm);
+//        timePickerAlarmTime = (TimePicker)findViewById(R.id.timePickerAlarmTime);
+        btnAddAlarm = (Button)findViewById(R.id.btn_add_activity);
         listViewAlarm	= (ListView)findViewById(R.id.listViewAlarm);
 
         sharedPref = getPreferences(Context.MODE_PRIVATE);
         sharedEditor = sharedPref.edit();
 
-        timePickerAlarmTime.setIs24HourView(false);
+//        timePickerAlarmTime.setIs24HourView(false);
 
         arrayAdapterAlarmList = new AlarmAdapter(mContext, arrayListAlarmTimeItem);
         listViewAlarm.setAdapter(arrayAdapterAlarmList);
 
         alarmManager = (AlarmManager)getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-        btnAddAlarm.setOnClickListener(new View.OnClickListener() {
 
+        btnAddAlarm.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
-                int hh = timePickerAlarmTime.getCurrentHour();
-                int mm = timePickerAlarmTime.getCurrentMinute();
-                int reqCode = DEFAULT_ALARM_REQUEST+arrayListAlarmTimeItem.size();
-                int i =arrayListAlarmTimeItem.size();
-
-                arrayListAlarmTimeItem.add(new AlarmData(hh, mm, reqCode));
-                arrayAdapterAlarmList.notifyDataSetChanged();
-
-                sharedEditor.putInt("list"+i+"hh", hh);
-                sharedEditor.putInt("list"+i+"mm", mm);
-                sharedEditor.putInt("list"+i+"reqCode", reqCode);
-                sharedEditor.putInt("size", i);
-                sharedEditor.commit();
-
-                GregorianCalendar gregorianCalendar = new GregorianCalendar(TimeZone.getTimeZone("GMT+09:00"));
-
-                int currentYY = currentCalendar.get(Calendar.YEAR);
-                int currentMM = currentCalendar.get(Calendar.MONTH);
-                int currentDD = currentCalendar.get(Calendar.DAY_OF_MONTH);
-
-                gregorianCalendar.set(currentYY, currentMM, currentDD, hh, mm,00);
-
-                if(gregorianCalendar.getTimeInMillis() < currentCalendar.getTimeInMillis()){
-                    gregorianCalendar.set(currentYY, currentMM, currentDD+1, hh, mm,00);
-                    Log.i("TAG",gregorianCalendar.getTimeInMillis()+":");
-                }
-
-
-                Intent intent = new Intent(AlarmSettingActivity.this, AlarmShowActivity.class);
-                intent.putExtra("time", hh+":"+mm);
-                intent.putExtra("data", "dd: " + currentCalendar.getTime().toLocaleString());
-                intent.putExtra("reqCode", reqCode);
-
-                //				Toast.makeText(mContext, "reqCode : "+reqCode, 0).show();
-
-                PendingIntent pi = PendingIntent.getActivity(AlarmSettingActivity.this, reqCode, intent,PendingIntent.FLAG_UPDATE_CURRENT );
-                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, gregorianCalendar.getTimeInMillis() ,AlarmManager.INTERVAL_DAY, pi);
+                Intent intent = new Intent(AlarmSettingActivity.this, AlarmAddActivity.class);
+//                startActivity(intent);
+                startActivityForResult(intent, 800);
             }
+
         });
+//        btnAddAlarm.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//                int hh = timePickerAlarmTime.getCurrentHour();
+//                int mm = timePickerAlarmTime.getCurrentMinute();
+//                int reqCode = DEFAULT_ALARM_REQUEST+arrayListAlarmTimeItem.size();
+//                int i =arrayListAlarmTimeItem.size();
+//
+//                arrayListAlarmTimeItem.add(new AlarmData(hh, mm, reqCode));
+//                arrayAdapterAlarmList.notifyDataSetChanged();
+//
+//                sharedEditor.putInt("list"+i+"hh", hh);
+//                sharedEditor.putInt("list"+i+"mm", mm);
+//                sharedEditor.putInt("list"+i+"reqCode", reqCode);
+//                sharedEditor.putInt("size", i);
+//                sharedEditor.commit();
+//
+//                GregorianCalendar gregorianCalendar = new GregorianCalendar(TimeZone.getTimeZone("GMT+09:00"));
+//
+//                int currentYY = currentCalendar.get(Calendar.YEAR);
+//                int currentMM = currentCalendar.get(Calendar.MONTH);
+//                int currentDD = currentCalendar.get(Calendar.DAY_OF_MONTH);
+//
+//                gregorianCalendar.set(currentYY, currentMM, currentDD, hh, mm,00);
+//
+//                if(gregorianCalendar.getTimeInMillis() < currentCalendar.getTimeInMillis()){
+//                    gregorianCalendar.set(currentYY, currentMM, currentDD+1, hh, mm,00);
+//                    Log.i("TAG",gregorianCalendar.getTimeInMillis()+":");
+//                }
+//
+//
+//                Intent intent = new Intent(AlarmSettingActivity.this, AlarmShowActivity.class);
+//                intent.putExtra("time", hh+":"+mm);
+//                intent.putExtra("data", "dd: " + currentCalendar.getTime().toLocaleString());
+//                intent.putExtra("reqCode", reqCode);
+//
+//                //				Toast.makeText(mContext, "reqCode : "+reqCode, 0).show();
+//
+//                PendingIntent pi = PendingIntent.getActivity(AlarmSettingActivity.this, reqCode, intent,PendingIntent.FLAG_UPDATE_CURRENT );
+//                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, gregorianCalendar.getTimeInMillis() ,AlarmManager.INTERVAL_DAY, pi);
+//            }
+//        });
 
     }
 
-    public void test(View view) {
-        Toast.makeText(this,"Floating button test", Toast.LENGTH_LONG).show();
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 800) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+
+                Intent intent = data;
+                String time = intent.getStringExtra("time");
+//                String data = intent.getStringExtra("data");
+                int reqCode = intent.getIntExtra("reqCode", 0);
+                Toast.makeText(this,"result "+time+"," +reqCode, Toast.LENGTH_LONG).show();
+//                Toast.makeText(this,"result "+reqCode, Toast.LENGTH_LONG).show();
+
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+
+                // Do something with the contact here (bigger example below)
+            }
+        }
     }
+
     @Override
     protected void onResume() {
         super.onResume();
