14c722132a256730d4511bea3b082986e204f191
ilgwonPark2 <ilgwon@interactor.com>
2018-11-26 13:37:41 +0900

    Revert "Revert "#24 Mission// permissionOnResult""
    This reverts commit 97ae03a
    
    fix #28 Mission// permissionOnResult

--- app/src/main/java/com/example/ilgwon/alarmbomb/user_interface/AlarmAddActivity.java
+++ app/src/main/java/com/example/ilgwon/alarmbomb/user_interface/AlarmAddActivity.java
@@ -1,9 +1,16 @@
 package com.example.ilgwon.alarmbomb.user_interface;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -56,11 +63,55 @@ public class AlarmAddActivity extends Activity {
                 intent.putExtra("minute", mm);
                 intent.putExtra("mission", mission_select);
                 intent.putExtra("reqCode", reqCode);
-
                 setResult(Activity.RESULT_OK, intent);
-                finish();
+                if (mission_select == "Decibel") {
+                    checkPermission();
+                } else {
+                    finish();
+                }
             }
         });
 
+    }
+
+    /**
+     * check permission to the user.
+     */
+    public void checkPermission() {
+        // Check 3 permissions to do this mission.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            // displaying AlertDialog with rationale
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder((AlarmAddActivity.this), 0);
+                builder.setTitle("AUDIO PERMISSION").setMessage("RECORD AUDIO, STORAGE ACCESS permission is needed to estimate decibel! Would you try to get permission again?")
+                        .setNegativeButton("No", null)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(AlarmAddActivity.this,
+                                        new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                                        7777);
+                            }
+                        });
+                builder.show();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                        7777);
+            }
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 7777) {
+            finish();
+        }
     }
 }
