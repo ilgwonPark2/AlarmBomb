aa88c3c671becdc0def5c8af45dc910667ac1bc9
ilgwonPark2 <ilgwon@interactor.com>
2018-11-30 11:02:47 +0900

    Fix #43 Main Activity - UI update

--- app/src/main/java/com/thenerds/ilgwon/alarmbomb/user_interface/MainActivity.java
+++ app/src/main/java/com/thenerds/ilgwon/alarmbomb/user_interface/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -28,7 +29,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private static final Pattern PASSWORD_PATTERN = Pattern.compile("^[a-zA-Z0-9!@.#$%^&*?_~]{4,16}$");
     private static final int RC_SIGN_IN = 900;
