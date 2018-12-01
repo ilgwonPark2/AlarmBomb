419890724a175ab1b589063c4f6b0c4a736cf3a1
ilgwonPark2 <ilgwon@interactor.com>
2018-12-01 22:19:57 +0900

    update

--- app/src/main/java/com/thenerds/ilgwon/alarmbomb/service/FirebaseMessagingService.java
+++ app/src/main/java/com/thenerds/ilgwon/alarmbomb/service/FirebaseMessagingService.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import com.google.firebase.messaging.RemoteMessage;
+import com.thenerds.ilgwon.alarmbomb.user_interface.AlarmSettingActivity;
 
 import java.util.Map;
 
@@ -19,28 +20,30 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         String message = data.get("content");
 
         //receive type1 알람 신청 승낙?
-        if (title == "invitation") {
+        if (title.equals("invitation")) {
             Log.i("invitation", "invitation");
-            startService(new Intent(getApplicationContext(), AlarmRingService.class));
+            Intent intent = new Intent(this, AlarmSettingActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         }
         //receive type2 승낙했음
-        if (title == "OK") {
+        if (title.equals("OK")) {
             Log.i(TAG, "OK");
 
             //알람세팅
         }
         //receive type3 거절했음
-        if (title == "NO") {
+        if (title.equals("NO")) {
             Log.i(TAG, "NO");
 
             //알람세팅 수정
         }
-        if (title == "Mission_fail") {
+        if (title.equals("Mission_fail")) {
             Log.i(TAG, "Mission_fail");
 
             //알람울리는 이벤트
         }
-        if (title == "Wake") {
+        if (title.equals("Wake")) {
             Log.i(TAG, "Wake");
 
             //알람끄기
