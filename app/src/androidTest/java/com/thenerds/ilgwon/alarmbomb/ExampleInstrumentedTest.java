4605adb0ecb2012f84080ba2878c5f10aa590bb6
hihyun <hyunhi1123@gmail.com>
2018-12-02 00:17:54 +0900

    Invitation

--- app/src/androidTest/java/com/thenerds/ilgwon/alarmbomb/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/thenerds/ilgwon/alarmbomb/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.ilgwon.alarmbomb", appContext.getPackageName());
+        assertEquals("com.thenerds.ilgwon.alarmbomb", appContext.getPackageName());
     }
 }
