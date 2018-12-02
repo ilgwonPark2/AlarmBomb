97f02f3e526214829b9bb5b3475c1611a0bf70f5
hihyun <hyunhi1123@gmail.com>
2018-12-02 12:07:17 +0900

    Invitation 완료

--- app/src/main/java/com/thenerds/ilgwon/alarmbomb/service/FirebaseMessagingService.java
+++ app/src/main/java/com/thenerds/ilgwon/alarmbomb/service/FirebaseMessagingService.java
@@ -1,9 +1,17 @@
 package com.thenerds.ilgwon.alarmbomb.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.RemoteMessage;
+import com.thenerds.ilgwon.alarmbomb.Model.NotificationModel;
+import com.thenerds.ilgwon.alarmbomb.R;
 import com.thenerds.ilgwon.alarmbomb.user_interface.AlarmReceivedActivity;
 import com.thenerds.ilgwon.alarmbomb.user_interface.AlarmSettingActivity;
 import com.thenerds.ilgwon.alarmbomb.user_interface.AlarmWakedActivity;
@@ -26,9 +34,11 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         //receive type1 알람 신청 승낙?
         if (title.equals("invitation")) {
             Log.i("invitation", "invitation");
-            Intent intent = new Intent(this, AlarmSettingActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
+            String friend=data.get("friend_name");
+            String time=data.get("time");
+            String code=data.get("code");
+            sendNotification(friend,time,code);
+
         }
         //receive type2 승낙했음
         if (title.equals("OK")) {
@@ -78,17 +88,25 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         }
         Log.i(TAG, title);
     }
-    private void sendNotification(){
+    private void sendNotification(String friend,String time,String code){
+//        Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com"));
         Intent intent=new Intent(this, Invitation.class);
-//        intent.putExtra();
-//        intent.putExtra();
-//        intent.putExtra();
-//        intent.putExtra();
-//        intent.putExtra();
-
-
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+        intent.putExtra("friend",friend);
+        intent.putExtra("time",time);
+        intent.putExtra("code",code);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent=PendingIntent.getActivity(this,1123,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.bomb)
+                .setContentTitle(friend + " send you to Alarm BOMB!")
+                .setContentText("at " + time + ". You AGREE?")
+                .setAutoCancel(true)
+                .setWhen(System.currentTimeMillis())
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1123,notificationBuilder.build());
+        //startActivity(intent);
 
 
     }
--- app/src/main/java/com/thenerds/ilgwon/alarmbomb/user_interface/Invitation.java
+++ app/src/main/java/com/thenerds/ilgwon/alarmbomb/user_interface/Invitation.java
@@ -1,19 +1,21 @@
 package com.thenerds.ilgwon.alarmbomb.user_interface;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.thenerds.ilgwon.alarmbomb.R;
 
-public class Invitation extends AppCompatActivity {
+public class Invitation extends Activity {
     Intent invitation;
     String Friend;
-    String hh;
-    String mm;
+    String time;
+    String code;
     TextView friend_name;
     TextView Time;
     Button yes;
@@ -22,20 +24,22 @@ public class Invitation extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.invitation);
+        Log.i("hi", "there");
+        invitation = getIntent();
         friend_name = findViewById(R.id.friendName);
         yes = findViewById(R.id.Yes);
         no = findViewById(R.id.No);
         Time = findViewById(R.id.time_sent);
-        invitation = getIntent();
-        hh = invitation.getStringExtra("hour");
-        mm = invitation.getStringExtra("minute");
+
+        time = invitation.getStringExtra("time");
         Friend = invitation.getStringExtra("friend");
+        code = invitation.getStringExtra("code");
+
         friend_name.setText(Friend);
-        Time.setText(hh + " : " + mm);
+        Time.setText(time);
 
-
-        super.onCreate(savedInstanceState);
     }
 
     public void yes_act() {
